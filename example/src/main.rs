c736e68dd51a0120d31cbe707f0d3508eb57f1b9
grorge <g97116@yahoo.com>
2024-07-11 13:23:38 +0800

    feat: add sd gguf
    Signed-off-by: grorge <g97116@yahoo.com>

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,10 +1,12 @@
 use wasmedge_stable_diffusion::stable_diffusion_interface::{ImageType, SdTypeT};
 use wasmedge_stable_diffusion::{BaseFunction, Context, Quantization, StableDiffusion, Task};
 fn main() {
-    let quantization =
-        Quantization::new("./sd-v1-4.ckpt", "sd-v1-4-Q8_0.gguf", SdTypeT::SdTypeQ8_0);
-    quantization.convert().unwrap();
-    let context = StableDiffusion::new(Task::TextToImage, "sd-v1-4-Q8_0.gguf");
+    // if you downloaded ckpt weights, you can use convert() to quantize the ckpt weight to gguf.
+    // For running other models, you need to change the model path of the following functions. 
+    // let quantization =
+    //     Quantization::new("./sd-v1-4.ckpt", "stable-diffusion-v1-4-Q8_0.gguf", SdTypeT::SdTypeQ8_0);
+    // quantization.convert().unwrap();
+    let context = StableDiffusion::new(Task::TextToImage, "stable-diffusion-v1-4-Q8_0.gguf");
     if let Context::TextToImage(mut text_to_image) = context.create_context().unwrap() {
         text_to_image
             .set_prompt("a lovely cat")
@@ -12,7 +14,7 @@ fn main() {
             .generate()
             .unwrap();
     }
-    let context = StableDiffusion::new(Task::ImageToImage, "sd-v1-4-Q8_0.gguf");
+    let context = StableDiffusion::new(Task::ImageToImage, "stable-diffusion-v1-4-Q8_0.gguf");
     if let Context::ImageToImage(mut image_to_image) = context.create_context().unwrap() {
         image_to_image
             .set_prompt("with blue eyes")
