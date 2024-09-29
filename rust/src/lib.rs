572cf47e2e7b8e496bf05b78c75b97ff7b040495
Xin Liu <sam@secondstate.io>
2024-09-29 11:46:16 +0900

    refactor: optimize the code structure
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -16,6 +16,26 @@ pub struct Quantization {
     pub output_path: String,
     pub wtype: SdTypeT,
 }
+impl Quantization {
+    pub fn new(model_path: &str, output_path: &str, wtype: SdTypeT) -> Quantization {
+        Quantization {
+            model_path: model_path.to_string(),
+            vae_model_path: "".to_string(),
+            output_path: output_path.to_string(),
+            wtype,
+        }
+    }
+    pub fn convert(&self) -> Result<(), WasmedgeSdErrno> {
+        unsafe {
+            stable_diffusion_interface::convert(
+                &self.model_path,
+                &self.vae_model_path,
+                &self.output_path,
+                self.wtype,
+            )
+        }
+    }
+}
 
 #[derive(Debug)]
 pub enum Task {
@@ -27,30 +47,6 @@ pub enum Task {
 pub enum Context {
     TextToImage(TextToImage),
     ImageToImage(ImageToImage),
-}
-
-#[derive(Debug)]
-pub struct StableDiffusion {
-    task: Task,
-    model_path: String,
-    clip_l_path: String,
-    t5xxl_path: String,
-    diffusion_model_path: String,
-    vae_path: String,
-    taesd_path: String,
-    control_net_path: String,
-    lora_model_dir: String,
-    embed_dir: String,
-    id_embed_dir: String,
-    vae_decode_only: bool,
-    vae_tiling: bool,
-    n_threads: i32,
-    wtype: SdTypeT,
-    rng_type: RngTypeT,
-    schedule: ScheduleT,
-    clip_on_cpu: bool,
-    control_net_cpu: bool,
-    vae_on_cpu: bool,
 }
 
 #[derive(Debug)]
@@ -202,39 +198,244 @@ pub trait BaseFunction {
     fn generate(&self) -> Result<(), WasmedgeSdErrno>;
 }
 
+/// Represents computation context for text-to-image task.
 #[derive(Debug)]
 pub struct TextToImage {
     pub common: BaseContext,
 }
+impl BaseFunction for TextToImage {
+    fn base(&mut self) -> &mut BaseContext {
+        &mut self.common
+    }
+    fn generate(&self) -> Result<(), WasmedgeSdErrno> {
+        if self.common.prompt.is_empty() {
+            return Err(WASMEDGE_SD_ERRNO_INVALID_ARGUMENT);
+        }
+        let mut data: Vec<u8> = vec![0; BUF_LEN as usize];
+        let result = unsafe {
+            stable_diffusion_interface::text_to_image(
+                &self.common.prompt,
+                self.common.session_id,
+                &self.common.control_image,
+                &self.common.negative_prompt,
+                self.common.guidance,
+                self.common.width,
+                self.common.height,
+                self.common.clip_skip,
+                self.common.cfg_scale,
+                self.common.sample_method,
+                self.common.sample_steps,
+                self.common.seed,
+                self.common.batch_count,
+                self.common.control_strength,
+                self.common.style_ratio,
+                self.common.normalize_input,
+                &self.common.input_id_images_dir,
+                self.common.canny_preprocess,
+                &self.common.upscale_model,
+                self.common.upscale_repeats,
+                &self.common.output_path,
+                data.as_mut_ptr(),
+                BUF_LEN,
+            )
+        };
+        result?;
+        Ok(())
+    }
+}
 
+/// Represents computation context for image-to-image task.
 #[derive(Debug)]
 pub struct ImageToImage {
     pub common: BaseContext,
     pub image: ImageType,
     pub strength: f32,
 }
-
-impl Quantization {
-    pub fn new(model_path: &str, output_path: &str, wtype: SdTypeT) -> Quantization {
-        Quantization {
-            model_path: model_path.to_string(),
-            vae_model_path: "".to_string(),
-            output_path: output_path.to_string(),
-            wtype,
-        }
+impl BaseFunction for ImageToImage {
+    fn base(&mut self) -> &mut BaseContext {
+        &mut self.common
     }
-    pub fn convert(&self) -> Result<(), WasmedgeSdErrno> {
-        unsafe {
-            stable_diffusion_interface::convert(
-                &self.model_path,
-                &self.vae_model_path,
-                &self.output_path,
-                self.wtype,
-            )
+    fn generate(&self) -> Result<(), WasmedgeSdErrno> {
+        if self.common.prompt.is_empty() {
+            return Err(WASMEDGE_SD_ERRNO_INVALID_ARGUMENT);
         }
+        match &self.image {
+            ImageType::Path(path) => {
+                if path.is_empty() {
+                    return Err(WASMEDGE_SD_ERRNO_INVALID_ARGUMENT);
+                }
+            }
+        }
+        let mut data: Vec<u8> = vec![0; BUF_LEN as usize];
+        let result = unsafe {
+            stable_diffusion_interface::image_to_image(
+                &self.image,
+                self.common.session_id,
+                self.common.guidance,
+                self.common.width,
+                self.common.height,
+                &self.common.control_image,
+                &self.common.prompt,
+                &self.common.negative_prompt,
+                self.common.clip_skip,
+                self.common.cfg_scale,
+                self.common.sample_method,
+                self.common.sample_steps,
+                self.strength,
+                self.common.seed,
+                self.common.batch_count,
+                self.common.control_strength,
+                self.common.style_ratio,
+                self.common.normalize_input,
+                &self.common.input_id_images_dir,
+                self.common.canny_preprocess,
+                &self.common.upscale_model,
+                self.common.upscale_repeats,
+                &self.common.output_path,
+                data.as_mut_ptr(),
+                BUF_LEN,
+            )
+        };
+        result?;
+        Ok(())
+    }
+}
+impl ImageToImage {
+    pub fn set_image(&mut self, image: ImageType) -> &mut Self {
+        {
+            self.image = image;
+        }
+        self
+    }
+    pub fn set_strength(&mut self, strength: f32) -> &mut Self {
+        {
+            self.strength = strength;
+        }
+        self
     }
 }
 
+/// Builder for creating a StableDiffusion instance.
+#[derive(Debug)]
+pub struct SDBuidler {
+    sd: StableDiffusion,
+}
+impl SDBuidler {
+    pub fn new(task: Task, model_path: impl AsRef<Path>) -> SDResult<Self> {
+        let path = model_path
+            .as_ref()
+            .to_str()
+            .ok_or_else(|| SDError::Operation("The model path is not valid unicode.".into()))?;
+        let sd = StableDiffusion::new(task, path);
+        Ok(Self { sd })
+    }
+
+    /// Create a new builder with a full model path.
+    pub fn new_with_full_model(task: Task, model_path: impl AsRef<Path>) -> SDResult<Self> {
+        let path = model_path
+            .as_ref()
+            .to_str()
+            .ok_or_else(|| SDError::Operation("The model path is not valid unicode.".into()))?;
+        let sd = StableDiffusion::new(task, path);
+        Ok(Self { sd })
+    }
+
+    /// Create a new builder with a standalone diffusion model.
+    pub fn new_with_standalone_model(
+        task: Task,
+        diffusion_model_path: impl AsRef<Path>,
+    ) -> SDResult<Self> {
+        let path = diffusion_model_path
+            .as_ref()
+            .to_str()
+            .ok_or_else(|| SDError::Operation("The model path is not valid unicode.".into()))?;
+        let sd = StableDiffusion::new_with_standalone_model(task, path);
+        Ok(Self { sd })
+    }
+
+    pub fn with_vae_path(mut self, path: impl AsRef<Path>) -> SDResult<Self> {
+        let path = path.as_ref().to_str().ok_or_else(|| {
+            SDError::InvalidPath("The path to the vae file is not valid unicode.".into())
+        })?;
+        self.sd.vae_path = path.into();
+        Ok(self)
+    }
+
+    pub fn with_clip_l_path(mut self, path: impl AsRef<Path>) -> SDResult<Self> {
+        let path = path.as_ref().to_str().ok_or_else(|| {
+            SDError::InvalidPath("The path to the clip_l file is not valid unicode.".into())
+        })?;
+        self.sd.clip_l_path = path.into();
+        Ok(self)
+    }
+
+    pub fn with_t5xxl_path(mut self, path: impl AsRef<Path>) -> SDResult<Self> {
+        let path = path.as_ref().to_str().ok_or_else(|| {
+            SDError::InvalidPath("The path to the t5xxl file is not valid unicode.".into())
+        })?;
+        self.sd.t5xxl_path = path.into();
+        Ok(self)
+    }
+
+    pub fn with_lora_model_dir(mut self, path: impl AsRef<Path>) -> SDResult<Self> {
+        let path = path.as_ref().to_str().ok_or_else(|| {
+            SDError::InvalidPath(
+                "The path to the lora model directory is not valid unicode.".into(),
+            )
+        })?;
+        self.sd.lora_model_dir = path.into();
+        Ok(self)
+    }
+
+    pub fn clip_on_cpu(mut self, enable: bool) -> Self {
+        self.sd.clip_on_cpu = enable;
+        self
+    }
+
+    pub fn vae_on_cpu(mut self, enable: bool) -> Self {
+        self.sd.vae_on_cpu = enable;
+        self
+    }
+
+    pub fn enable_vae_tiling(mut self, enable: bool) -> Self {
+        self.sd.vae_tiling = enable;
+        self
+    }
+
+    pub fn with_n_threads(mut self, n_threads: i32) -> Self {
+        self.sd.n_threads = n_threads;
+        self
+    }
+
+    pub fn build(self) -> StableDiffusion {
+        self.sd
+    }
+}
+
+/// Represents a stable diffusion model.
+#[derive(Debug)]
+pub struct StableDiffusion {
+    task: Task,
+    model_path: String,
+    clip_l_path: String,
+    t5xxl_path: String,
+    diffusion_model_path: String,
+    vae_path: String,
+    taesd_path: String,
+    control_net_path: String,
+    lora_model_dir: String,
+    embed_dir: String,
+    id_embed_dir: String,
+    vae_decode_only: bool,
+    vae_tiling: bool,
+    n_threads: i32,
+    wtype: SdTypeT,
+    rng_type: RngTypeT,
+    schedule: ScheduleT,
+    clip_on_cpu: bool,
+    control_net_cpu: bool,
+    vae_on_cpu: bool,
+}
 impl StableDiffusion {
     pub fn new(task: Task, model_path: &str) -> StableDiffusion {
         let vae_decode_only = match task {
@@ -357,205 +558,5 @@ impl StableDiffusion {
             self.lora_model_dir = lora_model_dir.to_string();
         }
         self
-    }
-}
-impl BaseFunction for TextToImage {
-    fn base(&mut self) -> &mut BaseContext {
-        &mut self.common
-    }
-    fn generate(&self) -> Result<(), WasmedgeSdErrno> {
-        if self.common.prompt.is_empty() {
-            return Err(WASMEDGE_SD_ERRNO_INVALID_ARGUMENT);
-        }
-        let mut data: Vec<u8> = vec![0; BUF_LEN as usize];
-        let result = unsafe {
-            stable_diffusion_interface::text_to_image(
-                &self.common.prompt,
-                self.common.session_id,
-                &self.common.control_image,
-                &self.common.negative_prompt,
-                self.common.guidance,
-                self.common.width,
-                self.common.height,
-                self.common.clip_skip,
-                self.common.cfg_scale,
-                self.common.sample_method,
-                self.common.sample_steps,
-                self.common.seed,
-                self.common.batch_count,
-                self.common.control_strength,
-                self.common.style_ratio,
-                self.common.normalize_input,
-                &self.common.input_id_images_dir,
-                self.common.canny_preprocess,
-                &self.common.upscale_model,
-                self.common.upscale_repeats,
-                &self.common.output_path,
-                data.as_mut_ptr(),
-                BUF_LEN,
-            )
-        };
-        result?;
-        Ok(())
-    }
-}
-
-impl BaseFunction for ImageToImage {
-    fn base(&mut self) -> &mut BaseContext {
-        &mut self.common
-    }
-    fn generate(&self) -> Result<(), WasmedgeSdErrno> {
-        if self.common.prompt.is_empty() {
-            return Err(WASMEDGE_SD_ERRNO_INVALID_ARGUMENT);
-        }
-        match &self.image {
-            ImageType::Path(path) => {
-                if path.is_empty() {
-                    return Err(WASMEDGE_SD_ERRNO_INVALID_ARGUMENT);
-                }
-            }
-        }
-        let mut data: Vec<u8> = vec![0; BUF_LEN as usize];
-        let result = unsafe {
-            stable_diffusion_interface::image_to_image(
-                &self.image,
-                self.common.session_id,
-                self.common.guidance,
-                self.common.width,
-                self.common.height,
-                &self.common.control_image,
-                &self.common.prompt,
-                &self.common.negative_prompt,
-                self.common.clip_skip,
-                self.common.cfg_scale,
-                self.common.sample_method,
-                self.common.sample_steps,
-                self.strength,
-                self.common.seed,
-                self.common.batch_count,
-                self.common.control_strength,
-                self.common.style_ratio,
-                self.common.normalize_input,
-                &self.common.input_id_images_dir,
-                self.common.canny_preprocess,
-                &self.common.upscale_model,
-                self.common.upscale_repeats,
-                &self.common.output_path,
-                data.as_mut_ptr(),
-                BUF_LEN,
-            )
-        };
-        result?;
-        Ok(())
-    }
-}
-impl ImageToImage {
-    pub fn set_image(&mut self, image: ImageType) -> &mut Self {
-        {
-            self.image = image;
-        }
-        self
-    }
-    pub fn set_strength(&mut self, strength: f32) -> &mut Self {
-        {
-            self.strength = strength;
-        }
-        self
-    }
-}
-
-#[derive(Debug)]
-pub struct SDBuidler {
-    sd: StableDiffusion,
-}
-impl SDBuidler {
-    pub fn new(task: Task, model_path: impl AsRef<Path>) -> SDResult<Self> {
-        let path = model_path
-            .as_ref()
-            .to_str()
-            .ok_or_else(|| SDError::Operation("The model path is not valid unicode.".into()))?;
-        let sd = StableDiffusion::new(task, path);
-        Ok(Self { sd })
-    }
-
-    /// Create a new builder with a full model path.
-    pub fn new_with_full_model(task: Task, model_path: impl AsRef<Path>) -> SDResult<Self> {
-        let path = model_path
-            .as_ref()
-            .to_str()
-            .ok_or_else(|| SDError::Operation("The model path is not valid unicode.".into()))?;
-        let sd = StableDiffusion::new(task, path);
-        Ok(Self { sd })
-    }
-
-    /// Create a new builder with a standalone diffusion model.
-    pub fn new_with_standalone_model(
-        task: Task,
-        diffusion_model_path: impl AsRef<Path>,
-    ) -> SDResult<Self> {
-        let path = diffusion_model_path
-            .as_ref()
-            .to_str()
-            .ok_or_else(|| SDError::Operation("The model path is not valid unicode.".into()))?;
-        let sd = StableDiffusion::new_with_standalone_model(task, path);
-        Ok(Self { sd })
-    }
-
-    pub fn with_vae_path(mut self, path: impl AsRef<Path>) -> SDResult<Self> {
-        let path = path.as_ref().to_str().ok_or_else(|| {
-            SDError::InvalidPath("The path to the vae file is not valid unicode.".into())
-        })?;
-        self.sd.vae_path = path.into();
-        Ok(self)
-    }
-
-    pub fn with_clip_l_path(mut self, path: impl AsRef<Path>) -> SDResult<Self> {
-        let path = path.as_ref().to_str().ok_or_else(|| {
-            SDError::InvalidPath("The path to the clip_l file is not valid unicode.".into())
-        })?;
-        self.sd.clip_l_path = path.into();
-        Ok(self)
-    }
-
-    pub fn with_t5xxl_path(mut self, path: impl AsRef<Path>) -> SDResult<Self> {
-        let path = path.as_ref().to_str().ok_or_else(|| {
-            SDError::InvalidPath("The path to the t5xxl file is not valid unicode.".into())
-        })?;
-        self.sd.t5xxl_path = path.into();
-        Ok(self)
-    }
-
-    pub fn with_lora_model_dir(mut self, path: impl AsRef<Path>) -> SDResult<Self> {
-        let path = path.as_ref().to_str().ok_or_else(|| {
-            SDError::InvalidPath(
-                "The path to the lora model directory is not valid unicode.".into(),
-            )
-        })?;
-        self.sd.lora_model_dir = path.into();
-        Ok(self)
-    }
-
-    pub fn clip_on_cpu(mut self, enable: bool) -> Self {
-        self.sd.clip_on_cpu = enable;
-        self
-    }
-
-    pub fn vae_on_cpu(mut self, enable: bool) -> Self {
-        self.sd.vae_on_cpu = enable;
-        self
-    }
-
-    pub fn enable_vae_tiling(mut self, enable: bool) -> Self {
-        self.sd.vae_tiling = enable;
-        self
-    }
-
-    pub fn with_n_threads(mut self, n_threads: i32) -> Self {
-        self.sd.n_threads = n_threads;
-        self
-    }
-
-    pub fn build(self) -> StableDiffusion {
-        self.sd
     }
 }
